2bfd25f28276f53d64651321e861419f38109859
srinidhidodo <srinidhi.nps@gmail.com>
2017-10-22 22:13:23 +0530

    Basic bookmark support

--- popup.js
+++ popup.js
@@ -2,12 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tab) { d = document;
 
       var f = d.createElement('form');
       f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
+      f.method = 'get';
       var i = d.createElement('input');
       i.type = 'hidden';
       i.name = 'url';
@@ -15,6 +14,13 @@ document.addEventListener('DOMContentLoaded', function() {
       f.appendChild(i);
       d.body.appendChild(f);
       f.submit();
+	  console.log(f.action);
+	  console.log("I am extension");
+	  var site=window.prompt("Enter a site name");
+	  var siteUrl="http://www."+site+".com";
+	  console.log(chrome.bookmarks.MAX_WRITE_OPERATIONS_PER_HOUR);
+	  chrome.bookmarks.create({url:siteUrl});
+	  //chrome.tabs.create({url:siteUrl});
     });
   }, false);
 }, false);
